4bb3be68ddf6cc91d6896961a948f8bd49e681a4
22005397 <22005397>
2022-05-15 14:15:49 +0200

    css (responsive) sur la page d'accueil

--- resources/views/home-page.blade.php
+++ resources/views/home-page.blade.php
@@ -8,21 +8,23 @@
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-<nav  >
-    <h1>MON SUPER SITE</h1>
-    <ul>
-        <li><a href={{route('login')}}>Connexion</a></li>
-        <li><a href={{route('inscription')}}>Inscription</a></li>
+<nav  class="bg-gray-400 flex sm:justify-between text-white px-5 h-16 items-center">
+    <h1 >MON SUPER SITE</h1>
+    <ul class="flex">
+        <li class="pl-16 hover:bg-gray-300 rounded"><a href={{route('login')}}>Connexion</a></li>
+        <li class="pl-5 hover:bg-gray-300 rounded"><a href={{route('inscription')}}>Inscription</a></li>
     </ul>
 </nav>
-<section>
-    <article>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus doloribus architecto quod ex aliquam placeat inventore amet pariatur voluptatibus quisquam dolorum et beatae cupiditate, fugiat ipsa enim. Voluptatum, consequuntur accusamus.</article>
-    <article>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus doloribus architecto quod ex aliquam placeat inventore amet pariatur voluptatibus quisquam dolorum et beatae cupiditate, fugiat ipsa enim. Voluptatum, consequuntur accusamus.</article>
-    <article>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus doloribus architecto quod ex aliquam placeat inventore amet pariatur voluptatibus quisquam dolorum et beatae cupiditate, fugiat ipsa enim. Voluptatum, consequuntur accusamus.</article>
-    <article>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus doloribus architecto quod ex aliquam placeat inventore amet pariatur voluptatibus quisquam dolorum et beatae cupiditate, fugiat ipsa enim. Voluptatum, consequuntur accusamus.</article>
-</section>
-<footer>
-    <span>Copyright © 2021 Mon-super-site.fr</span>
+
+<div class="flex flex-wrap flex-row justify-center text-sm py-4 px-32 ">
+    <span class="block text-gray-300 text-center bg-gray-700 py-16 px-8 " style="width:500px">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus doloribus architecto quod ex aliquam placeat inventore amet pariatur voluptatibus quisquam dolorum et beatae cupiditate, fugiat ipsa enim. Voluptatum, consequuntur accusamus.</span>
+    <span class="block text-gray-100 text-center bg-gray-500 py-16 px-8 " style="width:500px">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus doloribus architecto quod ex aliquam placeat inventore amet pariatur voluptatibus quisquam dolorum et beatae cupiditate, fugiat ipsa enim. Voluptatum, consequuntur accusamus.</span>
+    <span class="block text-gray-700 text-center bg-gray-300 py-16 px-8 " style="width:500px">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus doloribus architecto quod ex aliquam placeat inventore amet pariatur voluptatibus quisquam dolorum et beatae cupiditate, fugiat ipsa enim. Voluptatum, consequuntur accusamus.</span>
+    <span class="block text-gray-500 text-center bg-gray-100 py-16 px-8 " style="width:500px">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus doloribus architecto quod ex aliquam placeat inventore amet pariatur voluptatibus quisquam dolorum et beatae cupiditate, fugiat ipsa enim. Voluptatum, consequuntur accusamus.</span>
+</div>
+
+<footer class="p-4 bg-gray-100 text-center w-full  rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+    <span class=" items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">Copyright © 2021 Mon-super-site.fr</span>
 </footer>
 </body>
 </html>
